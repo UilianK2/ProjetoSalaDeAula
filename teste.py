5063d165e620a76f4e7e026f2a559b9b57f58051
UilianK2 <143419273+UilianK2@users.noreply.github.com>
2024-10-31 11:56:54 +0000

    add teste

---
+++ teste.py
@@ -0,0 +1,25 @@
+import os
+
+os.system("cls || clear")
+
+print ("Bom dia Sol")
+
+
+# COMANDO GIT
+
+#Atualiza o repositorio arual com dados do repositorio github
+#git pull 
+
+#Adiciona novos dados para o commit.
+# git add . ()
+
+#Versiona com os novos dados.
+# git commit -m "Titulo para o commit"
+
+#Atualiza trazendo commits do Github
+#git pull
+
+#Envia os commits para o GitHub
+#git push
+
+
